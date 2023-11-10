9b2709eebafc982e37f451e5d2ae9b5f5a8f9409
suyashLade <suyashlade@gmail.com>
2023-11-10 17:31:59 +0530

    db

--- src/util/dbConnect.ts
+++ src/util/dbConnect.ts
@@ -2,8 +2,7 @@ import { connect } from "mongoose";
 
 export default async function DbConnect() {
   await connect(
-    "mongodb+srv://suyashlade:TKgFObe6DRDPmS3z@kanban.vl712ho.mongodb.net/devlopment"
-  )
+    `${process.env.Db_Url}` )
     .then(() => {
       console.log(
         `⚡️[server]: Server is running at http://localhost:${process.env.PORT} `
